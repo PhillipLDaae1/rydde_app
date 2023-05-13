e93cf54b695f6c529df924fbe45594cf48c3051a
PhillipLDaae1 <e-phidaa@iskule.no>
2023-05-13 19:38:48 +0200

    Currently I have all the functionality I want working except adding points from tasks to totalPoints to each user.

--- src/components/DoneTasksList.vue
+++ src/components/DoneTasksList.vue
@@ -1,56 +1,77 @@
 <template>
   <div>
-    <h2>Oppgaver fullført av bruker {{ userID }}</h2>
+    <h2>Oppgaver fullført av bruker: {{ username }}</h2>
     <ul>
       <li v-for="task in doneTasks" :key="task.id">
-        {{ task.name }} - {{ task.points }} points
+        {{ task.taskName }} - {{ task.points }} poeng
       </li>
     </ul>
-    <p>Total Points: {{ totalPoints }}</p>
+    <p>Totale poeng: {{ totalPoints }}</p>
   </div>
 </template>
 
 <script>
-import { db } from '../firebase/firebase.js'
-import { collection, getDocs, doc, updateDoc, addDoc } from 'firebase/firestore';
-
+import { db } from '../firebase/firebase.js';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 
 export default {
-  props: {
-    userID: {
-      required: true
-    }
+  data() {
+    return {
+      userID: parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1"), 10),
+      doneTasks: [],
+      totalPoints: 0,
+      username: ""
+    };
   },
-  setup(props) {
-    const doneTasks = ref([])
-    const totalPoints = ref(0)
+  mounted() {
+    this.fetchDoneTasks();
+    this.fetchTotalPoints();
+    this.changeCookieToUsername();
+  },
+  methods: {
+    async fetchDoneTasks() {
+  const doneSnapshot = await getDocs(collection(db, 'done'));
+  const doneData = doneSnapshot.docs
+    .filter(doc => doc.data().userID === this.userID)
+    .map(doc => ({
+      id: doc.data().taskID,
+      date: doc.data().date.toDate(),
+    }));
 
-    const fetchDoneTasks = async () => {
-      const querySnapshot = await db
-        .collection('done')
-        .where('userID', '==', props.userID)
-        .get()
+  console.log('doneData:', doneData);
 
-      doneTasks.value = querySnapshot.docs.map((doc) => doc.data())
-    }
+  const tasksSnapshot = await getDocs(collection(db, 'tasks'));
+  const tasksData = tasksSnapshot.docs
+    .filter(doc => doneData.some(item => item.id === doc.data().id))
+    .map(doc => ({
+      id: doc.id,
+      taskName: doc.data().name,
+      points: doc.data().points,
+    }));
 
-    const fetchTotalPoints = async () => {
-      const doc = await db.collection('users').doc(props.userID).get()
+  console.log('tasksData:', tasksData);
 
-      if (doc.exists()) {
-        totalPoints.value = doc.data().totalPoints
+  this.doneTasks = tasksData;
+},
+
+
+    async fetchTotalPoints() {
+      const usersSnapshot = await getDocs(collection(db, 'users'));
+      const userDoc = usersSnapshot.docs.find(doc => doc.data().id === this.userID);
+      if (userDoc) {
+        this.totalPoints = userDoc.data().totalPoints;
+      }
+    },
+
+    async changeCookieToUsername() {
+      if (this.userID === 1) {
+        this.username = 'Phillip'
+      } else if (this.userID === 2) {
+        this.username = 'Per-Oskar'
+      } else {
+        this.username = 'Default Name'
       }
     }
-
-    onMounted(() => {
-      fetchDoneTasks()
-      fetchTotalPoints()
-    })
-
-    return {
-      doneTasks: computed(() => doneTasks.value),
-      totalPoints: computed(() => totalPoints.value)
-    }
   }
-}
+};
 </script>
--- src/components/TaskSelector.vue
+++ src/components/TaskSelector.vue
@@ -4,10 +4,64 @@
       <select class="task-dropdown" v-model="selectedTask">
         <option v-for="task in tasks" :value="task.id" :key="task.id">{{ task.name }} - {{ task.points }} poeng</option>
       </select>
-      <button class="mark-button" @click="markTaskAsDone(selectedTask)">Mark as Done</button>
+      <button class="mark-button" @click="markTaskAsDone(selectedTask)">Marker som gjort</button>
     </div>
   </template>
   
+  
+  
+  <script>
+  import { db } from '../firebase/firebase.js';
+  import { collection, getDocs, updateDoc, addDoc, doc } from 'firebase/firestore';
+  
+  export default {
+    data() {
+      return {
+        tasks: [],
+        selectedTask: null,
+      };
+    },
+    mounted() {
+      this.fetchTasks();
+    },
+    methods: {
+      async fetchTasks() {
+        try {
+          const querySnapshot = await getDocs(collection(db, 'tasks'));
+          this.tasks = querySnapshot.docs.map((doc) => doc.data());
+        } catch (error) {
+          console.error('Error fetching tasks:', error);
+        }
+      },
+      async markTaskAsDone(taskID) {
+        const userID = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1"), 10);
+  
+        try {
+          await addDoc(collection(db, 'done'), {
+            taskID,
+            userID,
+            date: new Date(),
+          });
+          console.log('Task marked as done successfully');
+          this.fetchTasks(); // Refresh the task list to update the view
+        } catch (error) {
+          console.error('Error marking task as done:', error);
+        }
+
+        window.location.reload(); 
+      },
+      async updateTotalPoints() {
+      const usersSnapshot = await getDocs(collection(db, 'users'));
+      const userDoc = usersSnapshot.docs.find(doc => doc.data().id === this.userID);
+      if (userDoc) {
+        this.totalPoints = userDoc.data().totalPoints;
+      }
+    },
+    },
+  };
+  </script>
+
+
   <style scoped>
 
   * {
@@ -49,47 +103,4 @@
   }
   
   </style>
-  
-  
-  <script>
-  import { db } from '../firebase/firebase.js';
-  import { collection, getDocs, updateDoc, addDoc, doc } from 'firebase/firestore';
-  
-  export default {
-    data() {
-      return {
-        tasks: [],
-        selectedTask: null,
-      };
-    },
-    mounted() {
-      this.fetchTasks();
-    },
-    methods: {
-      async fetchTasks() {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'tasks'));
-          this.tasks = querySnapshot.docs.map((doc) => doc.data());
-        } catch (error) {
-          console.error('Error fetching tasks:', error);
-        }
-      },
-      async markTaskAsDone(taskID) {
-        const userID = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1"), 10);
-  
-        try {
-          await addDoc(collection(db, 'done'), {
-            taskID,
-            userID,
-            date: new Date(),
-          });
-          console.log('Task marked as done successfully');
-          this.fetchTasks(); // Refresh the task list to update the view
-        } catch (error) {
-          console.error('Error marking task as done:', error);
-        }
-      },
-    },
-  };
-  </script>
   
