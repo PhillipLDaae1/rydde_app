edf26e8d0b4449c8a118fae9e6ea03fc2996d6bd
PhillipLDaae1 <e-phidaa@iskule.no>
2023-05-14 13:19:12 +0200

    When adding tasks as done, totalPoints for user is now updated

--- src/components/TaskSelector.vue
+++ src/components/TaskSelector.vue
@@ -1,65 +1,78 @@
 <template>
-    <div class="container">
-      <h2>Marker oppgaver som gjort</h2>
-      <select class="task-dropdown" v-model="selectedTask">
-        <option v-for="task in tasks" :value="task.id" :key="task.id">{{ task.name }} - {{ task.points }} poeng</option>
-      </select>
-      <button class="mark-button" @click="markTaskAsDone(selectedTask)">Marker som gjort</button>
-    </div>
-  </template>
-  
-  
-  
-  <script>
-  import { db } from '../firebase/firebase.js';
-  import { collection, getDocs, updateDoc, addDoc, doc } from 'firebase/firestore';
-  
-  export default {
-    data() {
-      return {
-        tasks: [],
-        selectedTask: null,
-      };
-    },
-    mounted() {
-      this.fetchTasks();
-    },
-    methods: {
-      async fetchTasks() {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'tasks'));
-          this.tasks = querySnapshot.docs.map((doc) => doc.data());
-        } catch (error) {
-          console.error('Error fetching tasks:', error);
-        }
-      },
-      async markTaskAsDone(taskID) {
-        const userID = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1"), 10);
-  
-        try {
-          await addDoc(collection(db, 'done'), {
-            taskID,
-            userID,
-            date: new Date(),
-          });
-          console.log('Task marked as done successfully');
-          this.fetchTasks(); // Refresh the task list to update the view
-        } catch (error) {
-          console.error('Error marking task as done:', error);
-        }
+  <div class="container">
+    <h2>Marker oppgaver som gjort</h2>
+    <select class="task-dropdown" v-model="selectedTask">
+      <option v-for="task in tasks" :value="task.id" :key="task.id">{{ task.name }} - {{ task.points }} poeng</option>
+    </select>
+    <button class="mark-button" @click="markTaskAsDone(selectedTask)">Marker som gjort</button>
+  </div>
+</template>
 
-        window.location.reload(); 
-      },
-      async updateTotalPoints() {
-      const usersSnapshot = await getDocs(collection(db, 'users'));
-      const userDoc = usersSnapshot.docs.find(doc => doc.data().id === this.userID);
-      if (userDoc) {
-        this.totalPoints = userDoc.data().totalPoints;
+<script>
+import { db } from '../firebase/firebase.js';
+import { collection, getDocs, updateDoc, addDoc, doc } from 'firebase/firestore';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+      selectedTask: null,
+    };
+  },
+  mounted() {
+    this.fetchTasks();
+  },
+  methods: {
+    async fetchTasks() {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'tasks'));
+        this.tasks = querySnapshot.docs.map((doc) => doc.data());
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
       }
     },
+    async markTaskAsDone(taskID) {
+      const userID = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1"), 10);
+  
+      try {
+        const doneTask = {
+          taskID,
+          userID,
+          date: new Date(),
+        };
+
+        // Add the task to the 'done' collection
+        await addDoc(collection(db, 'done'), doneTask);
+        console.log('Task marked as done successfully');
+
+        // Update the totalPoints for the respective user
+        const usersSnapshot = await getDocs(collection(db, 'users'));
+        const userDoc = usersSnapshot.docs.find(doc => doc.data().id === userID);
+
+        if (userDoc) {
+          const userRef = doc(db, 'users', userDoc.id);
+          const userPoints = userDoc.data().totalPoints || 0;
+          const taskPoints = this.tasks.find(task => task.id === taskID)?.points || 0;
+          const updatedPoints = userPoints + taskPoints;
+
+          // Update the totalPoints field in the user's document
+          await updateDoc(userRef, {
+            totalPoints: updatedPoints,
+          });
+
+          console.log('Total points updated successfully');
+        }
+
+        this.fetchTasks(); // Refresh the task list to update the view
+      } catch (error) {
+        console.error('Error marking task as done:', error);
+      }
+
+      window.location.reload();
     },
-  };
-  </script>
+  },
+};
+</script>
 
 
   <style scoped>
