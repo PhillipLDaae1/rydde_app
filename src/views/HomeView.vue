84d0e794314358b84d7f7f612af948ff830da44b
PhillipLDaae1 <e-phidaa@iskule.no>
2023-09-06 13:37:36 +0200

    Trying to fix pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,15 +28,59 @@ export default {
   
   data() {
     return {
-      username: ''
+      username: '',
+      greeting: ''
     }
   },
+
+  mounted() {
+    this.decidePromt()
+  },
+
+  methods: {
+    decidePromt() {
+      const currentHour = new Date().getHours();
+
+      let greeting;
+
+      if (currentHour >= 5 && currentHour < 12) {
+        this.greeting = "God morgen";
+      } else if (currentHour >= 12 && currentHour < 18) {
+        this.greeting = "God ettermiddag";
+      } else {
+        this.greeting = "God kveld";
+      }
+
+    }
+  }
 }
 </script>
 
 <template>
-  <div>
-    <h1>Velkommen, {{ username }}!</h1>
+  <div class="greeting">
+    <h1>{{ greeting }}, {{ username }}!</h1>
     <h2>Her var det visst tomt...</h2>
   </div>
+
+  <div class="info_card">
+    <h1>Dine poeng denne uken</h1>
+  </div>
 </template>
+
+<style scoped>
+@media (max-width: 600px) {
+  * {
+    font-size: 20px;
+  }
+  .greeting h1 {
+    font-size: 20px;
+  }
+
+  .greeting h2 {
+    font-size: 16px;
+    margin: 0 0 1rem 0;
+  }
+}
+
+
+</style>
